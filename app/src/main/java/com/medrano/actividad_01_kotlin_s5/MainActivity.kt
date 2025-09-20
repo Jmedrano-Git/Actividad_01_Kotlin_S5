4cfac209681d786f50296eda71e027f7ad8d1b20
Julio Medrano <julio.medrano@tecsup.edu.pe>
2025-09-19 20:19:13 -0500

    First Commit

---
+++ app/src/main/java/com/medrano/actividad_01_kotlin_s5/MainActivity.kt
@@ -0,0 +1,56 @@
+package com.medrano.actividad_01_kotlin_s5
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.medrano.actividad_01_kotlin_s5.ui.theme.Actividad_01_Kotlin_S5Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+        }
+    }
+}
+
+@Composable
+fun registroRapido() {
+    Column (
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(20.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        Text(
+            "Registro Rápido",
+
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+
+
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    registroRapido()
+}
