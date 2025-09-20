f98087fecbb13d8f9df4f4e1c8640f6d374ad2ff
Julio Medrano <julio.medrano@tecsup.edu.pe>
2025-09-19 21:29:31 -0500

    imports

--- app/src/main/java/com/medrano/actividad_01_kotlin_s5/MainActivity.kt
+++ app/src/main/java/com/medrano/actividad_01_kotlin_s5/MainActivity.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
