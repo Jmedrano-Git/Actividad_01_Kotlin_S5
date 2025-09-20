10fb7a3aa2a3c42569419c8f47526b605c39ba41
Julio Medrano <julio.medrano@tecsup.edu.pe>
2025-09-19 21:30:06 -0500

    Imports

--- app/src/main/java/com/medrano/actividad_01_kotlin_s5/MainActivity.kt
+++ app/src/main/java/com/medrano/actividad_01_kotlin_s5/MainActivity.kt
@@ -21,6 +21,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
